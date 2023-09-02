3acbb39db54b0ea19f5b18a2487555d1bcc9c38b
Forte-NaoP <bae9750@gmail.com>
2023-09-02 12:23:54 +0900

    [Medium] Time: 41 ms (58.60%), Space: 16.2 MB (75.09%) - LeetHub

--- LeetCode/Medium/1138-alphabet-board-path/1138-alphabet-board-path.py
+++ LeetCode/Medium/1138-alphabet-board-path/1138-alphabet-board-path.py
@@ -7,32 +7,33 @@ class Solution:
         
         cr, cc = 0, 0
         ans = ''
+        bc = ''
         z = False
         for ch in target:
             nr, nc = board[ch]
+            
+            if bc != 'z' and ch =='z':
+                nr -= 1
+                z = True
+            
             dr, dc = nr-cr, nc-cc
-            
-            if ch == 'z':
-                if dc < 0:
-                    ans += 'L'*abs(dc)
-                else:
-                    ans += 'R'*abs(dc)
-            
-                if dr < 0:
-                    ans += 'U'*abs(dr)
-                else:
-                    ans += 'D'*abs(dr)
+
+            if dr < 0:
+                ans += 'U'*abs(dr)
             else:
-                if dr < 0:
-                    ans += 'U'*abs(dr)
-                else:
-                    ans += 'D'*abs(dr)
-                    
-                if dc < 0:
-                    ans += 'L'*abs(dc)
-                else:
-                    ans += 'R'*abs(dc)
+                ans += 'D'*abs(dr)
+
+            if dc < 0:
+                ans += 'L'*abs(dc)
+            else:
+                ans += 'R'*abs(dc)
+                
+            if z:
+                nr += 1
+                ans += 'D'
+                z = False
             
             ans += '!'
             cr, cc = nr, nc
+            bc = ch
         return ans
