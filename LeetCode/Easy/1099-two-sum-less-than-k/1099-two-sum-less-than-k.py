23961eb9dc3e1c8071a7f3d8b91f709d8dba40f2
Forte-NaoP <bae9750@gmail.com>
2023-12-01 20:26:59 +0900

    [Easy] Time: 66 ms (18.05%), Space: 16.4 MB (17.32%) - LeetHub

---
+++ LeetCode/Easy/1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        twosum = -1
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] < k:
+                    if nums[i] + nums[j] > twosum:
+                        twosum = nums[i] + nums[j]
+                else:
+                    break
+                    
+        return twosum
+                
