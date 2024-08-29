07e00393039fb589b41b17e0d6d0e9260e7ac37c
Forte-NaoP <bae9750@gmail.com>
2024-08-29 12:41:12 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5088 ms, Memory: 365904 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,25 +1,46 @@
 import sys
-sys.setrecursionlimit(10**7)
+from collections import deque
+
 input = sys.stdin.readline
 
 n = int(input())
 tree = [[] for _ in range(n + 1)]
 visit = [False for _ in range(n + 1)]
+parent = [-1 for _ in range(n + 1)]
 dp = [[0, 0] for _ in range(n + 1)]
+dq = [[] for _ in range(n + 1)]
+max_depth = 0
+
 for _ in range(n - 1):
     a, b = map(int, input().split())
     tree[a].append(b)
     tree[b].append(a)
 
-def dfs(x):
-    visit[x] = True
-    dp[x][0] = 1
+q = deque([(1, 0)])
+visit[1] = True
 
-    for nxt in tree[x]:
+while q:
+    cur, depth = q.popleft()
+    dq[depth].append(cur)
+    max_depth = max(max_depth, depth)
+
+    for nxt in tree[cur]:
         if not visit[nxt]:
-            dfs(nxt)
-            dp[x][1] += dp[nxt][0]
-            dp[x][0] += min(dp[nxt])
+            q.append((nxt, depth + 1))
+            parent[nxt] = cur
+            visit[nxt] = True
 
-dfs(1)
+
+while max_depth >= 0 and dq[max_depth]:
+    cur = dq[max_depth].pop()
+
+    dp[cur][0] += 1
+    p = parent[cur]
+
+    dp[p][1] += dp[cur][0]
+    dp[p][0] += min(dp[cur])
+    
+    if not dq[max_depth]:
+        max_depth -= 1
+
 print(min(dp[1]))
