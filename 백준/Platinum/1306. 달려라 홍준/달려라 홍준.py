45d1123548113a750432aeba9cdd63ef37ca526a
Forte-NaoP <bae9750@gmail.com>
2024-09-17 21:39:55 +0900

    [Platinum V] Title: 달려라 홍준, Time: 4988 ms, Memory: 225272 KB -BaekjoonHub

--- 백준/Platinum/1306. 달려라 홍준/달려라 홍준.py
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.py
@@ -5,23 +5,36 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 ad = map(int, input().split())
-q = deque()
-ans = []
 
-for i in range(2 * m - 1):
+class SegTree:
+    def __init__(self, n) -> None:
+        self.base = 1
+        while n > 0:
+            self.base <<= 1
+            n >>= 1
+        self.n = self.base * 2
+        self.tree = [0 for _ in range(self.n)]
+
+    def update(self, idx, val):
+        idx += self.base
+        self.tree[idx] = val
+        idx >>= 1
+        while idx > 0:
+            self.tree[idx] = max(self.tree[idx << 1], self.tree[idx << 1 | 1])
+            idx >>= 1
+
+    def query(self, l, r, idx, s, e):
+        if r < s or e < l:
+            return 0
+        if l <= s and e <= r:
+            return self.tree[idx]
+        mid = (s + e) // 2
+        return max(self.query(l, r, idx << 1, s, mid), self.query(l, r, idx << 1 | 1, mid + 1, e))
+
+tree = SegTree(n)
+for i in range(n):
     nxt = next(ad)
-    while q and q[-1][0] < nxt:
-        q.pop()
-    q.append((nxt, i))
+    tree.update(i, nxt)
 
-ans.append(q[0][0])
-for i in range(2 * m - 1, n):
-    nxt = next(ad)
-    while q and q[-1][0] < nxt:
-        q.pop()
-    while q and q[0][1] <= i - 2 * m + 1:
-        q.popleft() 
-    q.append((nxt, i))
-    ans.append(q[0][0])
-
-print(*ans)
+for i in range(m - 1, n - m + 1):
+    print(tree.query(i - m + 1, i + m - 1, 1, 0, tree.base - 1), end=' ')
