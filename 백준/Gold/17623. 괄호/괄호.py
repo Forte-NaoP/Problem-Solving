d634dc5e56dacf48eb6850f3837185a1bb38fc71
Forte-NaoP <bae9750@gmail.com>
2024-10-11 16:58:51 +0900

    [Gold II] Title: 괄호, Time: 360 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17623. 괄호/괄호.py
+++ 백준/Gold/17623. 괄호/괄호.py
@@ -2,9 +2,9 @@ import sys
 
 input = sys.stdin.readline
 
+table = {1: '(', 2: ')', 3: '{', 4: '}', 5: '[', 6: ']'}
 def convert(x):
     res = []
-    table = {1: '(', 2: ')', 3: '{', 4: '}', 5: '[', 6: ']'}
     while x > 0:
         res.append(table[x % 10])
         x //= 10
@@ -13,27 +13,15 @@ def convert(x):
 
 dp = [[float('inf'), 1] for _ in range(1001)]
 dp[0][:] = 0, 0
+dp[1][:] = 12, 2
+dp[2][:] = 34, 2
+dp[3][:] = 56, 2
 
-a = lambda x, y: (min(12 * 10 ** y + x, 100 * x + 12), y + 2)
-b = lambda x, y: (min(34 * 10 ** y + x, 100 * x + 34), y + 2)
-c = lambda x, y: (min(56 * 10 ** y + x, 100 * x + 56), y + 2)
 d = lambda x, y: (10 ** (y + 1) + x * 10 + 2, y + 2)
 e = lambda x, y: (3 * 10 ** (y + 1) + x * 10 + 4, y + 2)
 f = lambda x, y: (5 * 10 ** (y + 1) + x * 10 + 6, y + 2)
 
-for i in range(1, 1001):
-    if i >= 1:
-        dmap = a(*dp[i - 1])
-        if dmap[0] < dp[i][0]:
-            dp[i][:] = dmap
-    if i >= 2:
-        dmap = b(*dp[i - 2])
-        if dmap[0] < dp[i][0]:
-            dp[i][:] = dmap
-    if i >= 3:
-        dmap = c(*dp[i - 3])
-        if dmap[0] < dp[i][0]:
-            dp[i][:] = dmap
+for i in range(4, 1001):
     if i % 2 == 0:
         dmap = d(*dp[i // 2])
         if dmap[0] < dp[i][0]:
@@ -46,6 +34,15 @@ for i in range(1, 1001):
         dmap = f(*dp[i // 5])
         if dmap[0] < dp[i][0]:
             dp[i][:] = dmap
+    for j in range(1, i // 2 + 1):
+        dmap = min(
+            dp[i - j][0] * 10 ** dp[j][1] + dp[j][0],
+            dp[j][0] * 10 ** dp[i - j][1] + dp[i - j][0]
+        )
+        digit = dp[i - j][1] + dp[j][1]
+        if dmap < dp[i][0]:
+            dp[i][:] = dmap, digit
 
 for _ in range(int(input())):
     print(convert(dp[int(input())][0]))
+
