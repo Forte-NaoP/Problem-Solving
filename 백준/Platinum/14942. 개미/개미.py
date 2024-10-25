04c31ad38cee430d3b0fbe84222a4ba4a5953283
Forte-NaoP <bae9750@gmail.com>
2024-10-26 06:24:49 +0900

    [Platinum V] Title: 개미, Time: 2880 ms, Memory: 245708 KB -BaekjoonHub

--- 백준/Platinum/14942. 개미/개미.py
+++ 백준/Platinum/14942. 개미/개미.py
@@ -5,29 +5,34 @@ input = sys.stdin.readline
 
 n = int(input())
 ant = [0] + [int(input()) for _ in range(n)]
-tree = defaultdict(dict)
+tree = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
     a, b, c = map(int, input().split())
-    tree[a][b] = c
-    tree[b][a] = c
+    tree[a].append((b, c))
+    tree[b].append((a, c))
 
 max_depth = 17
 parent = [[[0, 0] for _ in range(max_depth)] for _ in range(n + 1)]
-def init(cur, p):
-    parent[cur][0][0] = p
-    parent[cur][0][1] = tree[cur][p] if p != 0 else 0
 
-    for i in range(1, max_depth):
-        ith_parent, ith_cost = parent[cur][i - 1]
-        parent[cur][i][0] = parent[ith_parent][i - 1][0]
-        parent[cur][i][1] = parent[ith_parent][i - 1][1] + ith_cost
-    
-    for nxt in tree[cur].keys():
-        if nxt == p:
-            continue
-        init(nxt, cur)
+def init():
+    st = [(1, 0, 0)]
+    while st:
+        cur, p, cost = st.pop()
+        parent[cur][0][0] = p
+        parent[cur][0][1] = cost
 
-init(1, 0)
+        for i in range(1, max_depth):
+            ith_parent, ith_cost = parent[cur][i - 1]
+            if ith_parent == 0:
+                break
+            parent[cur][i][0] = parent[ith_parent][i - 1][0]
+            parent[cur][i][1] = parent[ith_parent][i - 1][1] + ith_cost
+        
+        for child, child_cost in tree[cur]:
+            if child != p:
+                st.append((child, cur, child_cost))
+
+init()
 
 for i in range(1, n + 1):
     cur = i
