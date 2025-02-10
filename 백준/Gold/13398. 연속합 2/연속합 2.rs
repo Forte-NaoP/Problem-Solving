42a699f7e8ae87668c568a98ef133d22a0fbb794
Forte-NaoP <bae9750@gmail.com>
2025-02-10 10:30:51 +0900

    [Gold V] Title: 연속합 2, Time: 4 ms, Memory: 15424 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.rs
@@ -0,0 +1,85 @@
+use std::cmp::{max, min, Reverse};
+use std::collections::{BinaryHeap, HashMap};
+use std::mem::swap;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+
+    let n = next!(usize);
+    let mut arr = vec![0; n];
+    for i in 0..n {
+        arr[i] = next!(i32);
+    }
+
+    let mut dp = [[0; 2]; 100_000];
+    let mut ans = arr[0];
+    dp[0][0] = arr[0];
+    dp[0][1] = arr[0];
+    for i in 1..n {
+        dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);
+        dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+        ans = max(ans, max(dp[i][0], dp[i][1]));
+    }
+    println!("{}", ans);
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
