0b91b7755ef4a10df271fb2f37aa9284d25018bf
Forte-NaoP <bae9750@gmail.com>
2025-02-20 12:41:07 +0900

    [Platinum V] Title: LCA 2, Time: 1216 ms, Memory: 110856 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.py
+++ 백준/Platinum/11438. LCA 2/LCA 2.py
@@ -1,55 +1,73 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 
-sys.setrecursionlimit(10**7)
-input = sys.stdin.readline
+def dfs_stack(root):
+    stack = [root]
+    visited[root] = True
+    depth_table[root] = 0
+    while stack:
+        node = stack.pop()
+        for child in graph[node]:
+            if not visited[child]:
+                visited[child] = True
+                depth_table[child] = depth_table[node] + 1
+                parent_table[child][0] = node
+                stack.append(child)
 
-n = int(input())
-tree = defaultdict(list)
-parent = [[0 for _ in range(18)] for _ in range(n + 1)]
-depth = [0 for _ in range(n + 1)]
 
-for _ in range(n - 1):
-    a, b = map(int, input().split())
-    tree[a].append(b)
-    tree[b].append(a)
+def prepare_lca(N):
+    for i in range(1, 20):
+        for node in range(1, N + 1):
+            if parent_table[node][i - 1] != -1:
+                parent_table[node][i] = parent_table[parent_table[node][i - 1]][i - 1]
 
-def init(x, p):
-    depth[x] = depth[p] + 1
-    parent[x][0] = p
 
-    for i in range(1, 18):
-        parent[x][i] = parent[parent[x][i - 1]][i - 1]
+def lca(node1, node2):
+    if depth_table[node1] < depth_table[node2]:
+        node1, node2 = node2, node1
 
-    for nxt in tree[x]:
-        if nxt == p:
-            continue
-        init(nxt, x)
+    diff = depth_table[node1] - depth_table[node2]
+    for i in range(20):
+        if (diff >> i) & 1:
+            node1 = parent_table[node1][i]
 
-init(1, 0)
+    if node1 == node2:
+        return node1
 
-def lca(a, b):
-    if a == 1 or b == 1:
-        return 1
-    if depth[a] < depth[b]:
-        a, b = b, a
-    
-    if depth[a] != depth[b]:
-        for i in range(17, -1, -1):
-            if depth[parent[a][i]] >= depth[b]:
-                a = parent[a][i]
+    for i in range(19, -1, -1):
+        if parent_table[node1][i] != parent_table[node2][i]:
+            node1 = parent_table[node1][i]
+            node2 = parent_table[node2][i]
 
-    ret = b
-    if a != b:
-        for i in range(17, -1, -1):
-            if parent[a][i] != parent[b][i]:
-                a = parent[a][i]
-                b = parent[b][i]
-            ret = parent[b][i]
-    return ret
-        
+    return parent_table[node1][0]
 
-m = int(input())
-for _ in range(m):
-    a, b = map(int, input().split())
-    print(lca(a, b))
+
+input = sys.stdin.read
+data = input().split()
+
+N = int(data[0])
+graph = defaultdict(list)
+index = 1
+
+for _ in range(N - 1):
+    u, v = int(data[index]), int(data[index + 1])
+    graph[u].append(v)
+    graph[v].append(u)
+    index += 2
+
+visited = [False] * (N + 1)
+parent_table = [[-1] * 20 for _ in range(N + 1)]
+depth_table = [0] * (N + 1)
+
+dfs_stack(1)
+prepare_lca(N)
+
+M = int(data[index])
+index += 1
+output = []
+for _ in range(M):
+    node1, node2 = int(data[index]), int(data[index + 1])
+    output.append(str(lca(node1, node2)))
+    index += 2
+
+sys.stdout.write("\n".join(output) + "\n")
