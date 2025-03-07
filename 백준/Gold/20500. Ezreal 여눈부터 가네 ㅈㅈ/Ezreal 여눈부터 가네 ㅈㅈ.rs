c63410e8b3db9a0fbdc6091a607fcb0c7a5134a9
Forte-NaoP <bae9750@gmail.com>
2025-03-07 14:50:26 +0900

    [Gold V] Title: Ezreal 여눈부터 가네 ㅈㅈ, Time: 0 ms, Memory: 13344 KB -BaekjoonHub

---
+++ 백준/Gold/20500. Ezreal 여눈부터 가네 ㅈㅈ/Ezreal 여눈부터 가네 ㅈㅈ.rs
@@ -0,0 +1,83 @@
+use std::mem::swap;
+use std::cmp::{max, min, Ordering};
+use std::collections::{HashMap, VecDeque, BinaryHeap};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+
+    let n = next!(usize);
+    let mut dp = [0; 1557];
+    dp[2] = 1;
+    for i in 3..=n {
+        if i % 2 == 1 {
+            dp[i] = (dp[i - 1] * 2 - 1) % 1_000_000_007;
+        } else {
+            dp[i] = (dp[i - 1] * 2 + 1) % 1_000_000_007;
+        }
+    }
+
+    println!("{}", dp[n]);
+
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
