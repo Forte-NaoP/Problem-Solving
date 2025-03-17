9e406caedc44064e88d4d03f2b3d05d25ad35a1e
Forte-NaoP <bae9750@gmail.com>
2025-03-17 14:40:57 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 4 ms, Memory: 16428 KB -BaekjoonHub

---
+++ 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.rs
@@ -0,0 +1,99 @@
+use std::iter::Rev;
+use std::mem::swap;
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+
+    let n = next!(usize);
+    let mut st: Vec<(i64, i64)> = vec![];
+    let mut width = 0;
+    let mut ans = 0;
+    for _ in 0..n {
+        width = 0;
+        let cur = next!(i64);
+        while let Some(&top) = st.last() {
+            if top.0 <= cur {
+                break;
+            }
+            width += top.1;
+            ans = ans.max(width * top.0);
+            st.pop();
+        }
+        width += cur;
+        st.push((cur, width));
+    }
+
+    width = 0;
+    while let Some(&top) = st.last() {
+        width += top.1;
+        ans = ans.max(width * top.0);
+        st.pop();
+    }
+
+    println!("{}", ans);
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
