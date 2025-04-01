d67396852ff6f8d4d729c0e025d5e35eae64dae4
Forte-NaoP <bae9750@gmail.com>
2025-04-01 15:43:48 +0900

    [Bronze II] Title: 보물 지도, Time: 12 ms, Memory: 17448 KB -BaekjoonHub

---
+++ 백준/Bronze/29332. 보물 지도/보물 지도.rs
@@ -0,0 +1,90 @@
+use std::iter::Rev;
+use std::mem::swap;
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+    
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+    
+    let n = next!(usize);
+    let (mut l, mut r, mut u, mut d) = (i64::MIN, i64::MAX, i64::MAX, i64::MIN);
+    for _ in 0..n {
+        let (x, y, c) = next!(i64, i64, char);
+        match c {
+            'U' => d = d.max(y),
+            'D' => u = u.min(y),
+            'L' => r = r.min(x),
+            'R' => l = l.max(x),
+            _ => unreachable!(),
+        };
+    }
+    
+    if l == i64::MIN || r == i64::MAX || u == i64::MAX || d == i64::MIN {
+        println!("Infinity");
+    } else {
+        println!("{}", (r - l - 1) * (u - d - 1));
+    }
+
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
