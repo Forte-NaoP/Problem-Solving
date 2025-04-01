2cbfbf519cee4b71d3ee6d99eb2d30173e729ce6
Forte-NaoP <bae9750@gmail.com>
2025-04-01 16:58:18 +0900

    [Silver IV] Title: 단어 개수 세기, Time: 0 ms, Memory: 13348 KB -BaekjoonHub

---
+++ 백준/Silver/19844. 단어 개수 세기/단어 개수 세기.rs
@@ -0,0 +1,89 @@
+use std::iter::Rev;
+use std::mem::swap;
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+    
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+    
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'h'];
+    let prefix = ["c", "j", "n", "m", "t", "s", "l", "l", "d", "qu", "s"];
+
+    let space: Vec<_> = stdin.split(' ').map(|x| x.split('-')).flatten().collect();
+    let mut ans = 0;
+    for &w in space.iter() {
+        if let Some(idx) = w.find('\'') {
+            if prefix.contains(&&w[..idx]) && vowels.contains(&w.chars().nth(idx + 1).unwrap()) {
+                ans += 2;
+            } else {
+                ans += 1;
+            }
+        } else {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
