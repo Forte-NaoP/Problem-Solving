35adb32c61160f8c706534da857db8d118458b08
Forte-NaoP <bae9750@gmail.com>
2025-04-01 15:06:12 +0900

    [Bronze II] Title: 우리 집 밑에 편의점이 있는데, Time: 0 ms, Memory: 13344 KB -BaekjoonHub

---
+++ 백준/Bronze/20155. 우리 집 밑에 편의점이 있는데/우리 집 밑에 편의점이 있는데.rs
@@ -0,0 +1,77 @@
+use std::iter::Rev;
+use std::mem::swap;
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+    
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+    
+    let mut cnt = [0; 1001];
+    let (n, m) = next!(usize, usize);
+    for _ in 0..n {
+        cnt[next!(usize)] += 1;
+    }
+    println!("{}", cnt.iter().max().unwrap());
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
