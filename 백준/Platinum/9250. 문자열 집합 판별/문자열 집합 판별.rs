8b62eb2a71313fd9e6dfe981aa6a06905a40faf3
Forte-NaoP <bae9750@gmail.com>
2025-04-11 15:29:18 +0900

    [Platinum II] Title: 문자열 집합 판별, Time: 140 ms, Memory: 51784 KB -BaekjoonHub

---
+++ 백준/Platinum/9250. 문자열 집합 판별/문자열 집합 판별.rs
@@ -0,0 +1,182 @@
+use std::cell::Ref;
+use std::iter::Rev;
+use std::mem::swap;
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::pin::Pin;
+use std::{default, i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+use std::rc::{Rc, Weak};
+
+struct TrieNode {
+    children: [Option<Rc<RefCell<TrieNode>>>; 26],
+    fail_link: Option<Weak<RefCell<TrieNode>>>,
+    is_end: bool,
+}
+
+impl TrieNode {
+    fn new() -> Self {
+        TrieNode {
+            children: Default::default(),
+            fail_link: None,
+            is_end: false,
+        }
+    }
+}
+
+struct Trie {
+    root: Rc<RefCell<TrieNode>>,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie {
+            root: Rc::new(RefCell::new(TrieNode::new())),
+        }
+    }
+
+    fn insert(&mut self, word: &str) {
+        let mut node = self.root.clone();
+        for c in word.chars() {
+            let idx = c as usize - 'a' as usize;
+            node = node.clone()
+                .borrow_mut()
+                .children[idx]
+                .get_or_insert(Rc::new(RefCell::new(TrieNode::new())))
+                .clone();
+        }
+        node.borrow_mut().is_end = true;
+    }
+
+    fn search(&self, word: &str) -> bool {
+        let mut node = self.root.clone();
+        for c in word.chars() {
+            let idx = c as usize - 'a' as usize;
+            while !Rc::ptr_eq(&node, &self.root) && node.borrow().children[idx].is_none() {
+                node = node.clone().borrow().fail_link.as_ref().unwrap().upgrade().unwrap();
+            }
+
+            if node.borrow().children[idx].is_some() {
+                node = node.clone().borrow().children[idx].as_ref().unwrap().clone();
+            }
+
+            if node.borrow().is_end {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn failure(&mut self) {
+        let mut dq = VecDeque::new();
+        dq.push_back(self.root.clone());
+
+        while let Some(node) = dq.pop_front() {
+            for idx in 0..26 {
+                if let Some(child) = node.clone().borrow().children[idx].clone() {
+                    if Rc::ptr_eq(&node, &self.root) {
+                        child.borrow_mut().fail_link = Some(Rc::downgrade(&self.root));
+                    } else {
+                        let mut parent = node.borrow().fail_link.as_ref().unwrap().upgrade().unwrap();
+                        while !Rc::ptr_eq(&parent, &self.root) && parent.borrow().children[idx].is_none() {
+                            parent = parent.clone().borrow().fail_link.as_ref().unwrap().upgrade().unwrap();
+                        }
+                        
+                        if parent.borrow().children[idx].is_some() {
+                            parent = parent.clone().borrow().children[idx].as_ref().unwrap().clone();
+                        }
+
+                        child.borrow_mut().fail_link = Some(Rc::downgrade(&parent));
+                    }
+
+                    if child.clone().borrow().fail_link.as_ref().unwrap().upgrade().unwrap().borrow().is_end {
+                        child.borrow_mut().is_end = true;
+                    }
+
+                    dq.push_back(child.clone());
+                }
+            }
+        }
+
+    }
+}
+
+
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+    
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+
+    let mut trie = Trie::new();
+    let n = next!(usize);
+    for _ in 0..n {
+        trie.insert(next!());
+    }
+    trie.failure();
+    let m = next!(usize);
+    for _ in 0..m {
+        match trie.search(next!()) {
+            true => println!("YES"),
+            false => println!("NO")
+        }
+    }
+
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        });
+    };
+}
