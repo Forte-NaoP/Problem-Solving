d873ed6eef540d5f85d675b24eb14eb989d19de9
Forte-NaoP <bae9750@gmail.com>
2025-05-02 10:22:50 +0900

    [Platinum V] Title: Число, Time: 0 ms, Memory: 13352 KB -BaekjoonHub

---
+++ 백준/Platinum/21479. Число/Число.rs
@@ -0,0 +1,81 @@
+use std::collections::VecDeque;
+use std::mem::swap;
+use std::rc::Rc;
+use std::{i32, usize, vec};
+use std::{
+    io::{Write, BufWriter, StdoutLock}, cell::RefCell
+};
+
+fn solve(stdin: &str) {
+    let mut tokens = stdin.split_ascii_whitespace();
+    
+    macro_rules! next {
+        () => {
+            tokens.next().unwrap()
+        };
+
+        ($t:ty) => {
+            tokens.next().unwrap().parse::<$t>().unwrap()
+        };
+
+        ($t:ty, $($ts:ty),+) => {
+            (
+                tokens.next().unwrap().parse::<$t>().unwrap(),
+                $(tokens.next().unwrap().parse::<$ts>().unwrap()),+
+            )
+        };
+    }
+
+    let mut v: Vec<&str> = tokens.collect();
+    v.sort_by(|a, b| {
+        let ab = format!("{}{}", a, b);
+        let ba = format!("{}{}", b, a);
+        ba.cmp(&ab)
+    });
+
+    if v[0] == "0" {
+        println!("0");
+    } else {
+        println!("{}", v.join(""));
+    }
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    solve(&stdin);
+    STDOUT.with(|refcell| {
+        Write::flush(&mut *refcell.borrow_mut()).unwrap();
+    });
+}   
+
+
+/* https://blog.kiwiyou.dev/posts/rust-fastio/ */
+thread_local! {
+    static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> 
+        = RefCell::new(
+            BufWriter::with_capacity(
+                1 << 17, 
+                std::io::stdout().lock()
+            )
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            writeln!(refcell.borrow_mut(), $($t)*).unwrap();
+        })
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($t:tt)*) => {
+        STDOUT.with(|refcell| {
+            use std::io::*;
+            write!(refcell.borrow_mut(), $($t)*).unwrap();
+        })
+    };
+}
